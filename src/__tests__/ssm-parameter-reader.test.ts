9032da75b67d4d5ffa1b518034cf3150091c398a
Henrik Steen <henrist@henrist.net>
2020-06-13 14:51:47 +0200

    feat: add SsmParameterReader

---
+++ src/__tests__/ssm-parameter-reader.test.ts
@@ -0,0 +1,25 @@
+import { CfnParameter, ParameterType } from "@aws-cdk/aws-ssm"
+import { App, Stack } from "@aws-cdk/core"
+import "jest-cdk-snapshot"
+import { SsmParameterReader } from "../ssm-parameter-reader"
+
+test("ssm-parameter-reader", () => {
+  const app = new App()
+  const stack1 = new Stack(app, "Stack1")
+  const parameterName = "/my/param"
+
+  new CfnParameter(stack1, "Param", {
+    type: ParameterType.STRING,
+    name: parameterName,
+    value: "test",
+  })
+
+  const stack2 = new Stack(app, "Stack2")
+  new SsmParameterReader(stack2, "ParamReader", {
+    parameterName,
+    region: "eu-west-1",
+    digest: "123",
+  })
+
+  expect(stack2).toMatchCdkSnapshot()
+})
---
+++ src/ssm-parameter-reader.ts
@@ -0,0 +1,57 @@
+import * as cdk from "@aws-cdk/core"
+import * as cr from "@aws-cdk/custom-resources"
+
+interface Props {
+  parameterName: string
+  region: string
+  /**
+   * Value that must be updated to check if the parameter has a new value.
+   *
+   * @default Date.now().toString()
+   */
+  digest?: string
+}
+
+function removeLeadingSlash(value: string): string {
+  return value.slice(0, 1) == "/" ? value.slice(1) : value
+}
+
+/**
+ * Get value of a SSM parameter dynamically during deployment
+ * with support to read cross-region.
+ */
+export class SsmParameterReader extends cr.AwsCustomResource {
+  constructor(scope: cdk.Construct, id: string, props: Props) {
+    super(scope, id, {
+      onUpdate: {
+        service: "SSM",
+        action: "getParameter",
+        parameters: {
+          Name: props.parameterName,
+        },
+        region: props.region,
+        // Update physical id to fetch the latest version.
+        physicalResourceId: cr.PhysicalResourceId.of(
+          props.digest ?? Date.now().toString(),
+        ),
+      },
+      policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [
+          cdk.Arn.format(
+            {
+              service: "ssm",
+              region: props.region,
+              resource: "parameter",
+              resourceName: removeLeadingSlash(props.parameterName),
+            },
+            cdk.Stack.of(scope),
+          ),
+        ],
+      }),
+    })
+  }
+
+  public getParameterValue(): string {
+    return this.getResponseField("Parameter.Value")
+  }
+}
