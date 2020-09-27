5030f2492144090ddff477288a79274897853afb
Henrik Steen <henrist@henrist.net>
2020-09-28 00:48:16 +0200

    feat(tags): relax scope and support stages
    This is needed to be able to tag stages which do not
    propagate tags from the root application.

--- src/tags.ts
+++ src/tags.ts
@@ -6,19 +6,23 @@ import * as cdk from "@aws-cdk/core"
  * CDK will propagate tags too all resources that support it.
  */
 export function tagResources(
-  app: cdk.App,
+  scope: cdk.Construct,
   tags: (stack: cdk.Stack) => Record<string, string>,
 ): void {
   // Keep the deprecated code to remain compatible with older CDK versions.
   // We will resolve this deprecation later.
   // eslint-disable-next-line deprecation/deprecation
-  app.node.applyAspect({
+  scope.node.applyAspect({
     visit(construct: cdk.IConstruct) {
       if (cdk.TagManager.isTaggable(construct)) {
-        const stack = construct.node.scopes.find((it): it is cdk.Stack =>
+        // We pick the last stack in chain to support stages where
+        // there are multiple stacks.
+        const allStacks = construct.node.scopes.filter((it): it is cdk.Stack =>
           cdk.Stack.isStack(it),
         )
 
+        const stack =
+          allStacks.length > 0 ? allStacks[allStacks.length - 1] : undefined
         if (stack != null) {
           for (const [key, value] of Object.entries(tags(stack))) {
             construct.tags.setTag(key, value, 100, true)
