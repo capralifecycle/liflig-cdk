d00a3316cb9a9c27bd69ddcd0473e1e67d528bbe
Henrik Steen <henrist@henrist.net>
2020-11-16 22:37:20 +0100

    feat(snapshots): remove assets details from modern bootstraped app

--- src/snapshots.ts
+++ src/snapshots.ts
@@ -4,8 +4,8 @@
 import * as cpy from "cpy"
 import * as del from "del"
 import * as fs from "fs"
-import * as path from "path"
 import * as glob from "glob"
+import * as path from "path"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function removeVersion(data: any): any {
@@ -87,7 +87,7 @@ function removeAssetDetailsFromTemplate(data: any): any {
           } else if (
             key === "aws:asset:path" &&
             typeof value === "string" &&
-            value.startsWith("asset.")
+            /asset\.[0-9a-f]{64}/.test(value)
           ) {
             return [key, "asset.snapshot-value"]
           } else {
@@ -103,6 +103,9 @@ function removeAssetDetailsFromTemplate(data: any): any {
     if (newCurrentVersion) {
       return newCurrentVersion
     }
+
+    // Handle typical content hashes.
+    return data.replace(/[0-9a-f]{64}/g, "snapshot-value")
   }
 
   return data
@@ -141,6 +144,9 @@ function removeAssetDetailsFromManifest(data: any): any {
     if (newCurrentVersion) {
       return newCurrentVersion
     }
+
+    // Handle typical content hashes.
+    return data.replace(/[0-9a-f]{64}/g, "snapshot-value")
   }
 
   return data
@@ -235,6 +241,10 @@ export async function createCloudAssemblySnapshot(
 
   // Remove asset contents for now.
   await del(path.join(dst, "**/asset.*"))
+
+  // Remove asset configs so we don't have to update
+  // snapshots for asset changes.
+  await del(path.join(dst, "**/*.assets.json"))
 
   // Transform the manifest to be more snapshot friendly.
   for (const file of glob.sync("**/manifest.json", { cwd: base })) {
