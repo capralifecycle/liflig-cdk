1ee3740f99bd0a1e2f3ff0150ba7fec56d481779
Kristian Rekstad <kre@capraconsulting.no>
2022-07-07 15:11:49 +0200

    feat(ecs): add open telemetry sidecar construct
    This will create a sidecar on a FargateService construct,
    which collects open telemetry-data from the service's instrumentation
    agent.
    The data is forwarded to CloudWatch Metrics and XRay.

--- src/ecs/index.ts
+++ src/ecs/index.ts
@@ -1,3 +1,4 @@
 export { Cluster, ClusterProps } from "./cluster"
 export { FargateService, FargateServiceProps } from "./fargate-service"
 export { ListenerRule, ListenerRuleProps } from "./listener-rule"
+export { OpenTelemetryCollectors, SidecarOptions } from "./open-telemetry"
