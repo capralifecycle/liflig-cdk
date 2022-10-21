7debf31d659decdea6ff7ea251753612cebad145
stekern <stekern@users.noreply.github.com>
2022-10-21 12:09:27 +0200

    feat: update alarms for ecs services

--- src/alarms/service-alarms.ts
+++ src/alarms/service-alarms.ts
@@ -1,10 +1,16 @@
-import * as constructs from "constructs"
+import * as cdk from "aws-cdk-lib"
 import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch"
 import * as logs from "aws-cdk-lib/aws-logs"
-import * as cdk from "aws-cdk-lib"
+import * as constructs from "constructs"
 
 export interface ServiceAlarmsProps extends cdk.StackProps {
+  /**
+   * The default action to use for CloudWatch alarm state changes
+   */
   action: cloudwatch.IAlarmAction
+  /**
+   * The name of the ECS service.
+   */
   serviceName: string
 }
 
@@ -39,9 +45,15 @@ export class ServiceAlarms extends constructs.Construct {
   addJsonErrorAlarm(props: {
     logGroup: logs.ILogGroup
     alarmDescription?: string
-    /** Set to `false` to stop the alarm from sending OK events.
-     * @default true */
+    /**
+     * Set to `false` to stop the alarm from sending OK events.
+     * @default true
+     * */
     enableOkAction?: boolean
+    /**
+     * An action to use for CloudWatch alarm state changes instead of the default action
+     */
+    action?: cloudwatch.IAlarmAction
   }): void {
     const errorMetricFilter = props.logGroup.addMetricFilter(
       "ErrorMetricFilter",
@@ -79,58 +91,233 @@ export class ServiceAlarms extends constructs.Construct {
         treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
       })
 
-    errorAlarm.addAlarmAction(this.action)
+    errorAlarm.addAlarmAction(props.action ?? this.action)
     if (props.enableOkAction ?? true) {
-      errorAlarm.addOkAction(this.action)
+      errorAlarm.addOkAction(props.action ?? this.action)
     }
   }
 
   /**
-   * Monitor healthy host count and connection errors from load balancer.
+   * Sets up three CloudWatch Alarms for monitoring an ECS service behind a target group:
+   * 1) one that triggers if the target is responding with too many 5xx errors.
+   * 2) one that triggers if the 95% percentile of response times from the target is too high.
+   * 3) one that triggers if there are no healthy targets or if the load balancer fails to connect to targets.
    */
-  addTargetGroupAlarm(props: {
+  addTargetGroupAlarms(props: {
+    /**
+     * The full name of the target group.
+     */
     targetGroupFullName: string
+    /**
+     * The full name of the application load balancer.
+     */
     loadBalancerFullName: string
+    /**
+     * Configuration for a composite alarm.
+     *
+     * @default Configured with sane defaults.
+     */
+    targetHealthAlarm?: {
+      /**
+       * @default true
+       */
+      enabled?: boolean
+      /**
+       * An action to use for CloudWatch alarm state changes instead of the default action
+       */
+      action?: cloudwatch.IAlarmAction
+      /**
+       * @default 60 seconds
+       */
+      period?: cdk.Duration
+      /**
+       * @default 1
+       */
+      evaluationPeriods?: number
+      /**
+       * @default 1
+       */
+      threshold?: number
+      description?: string
+    }
+    /**
+     * Configuration for an alarm.
+     *
+     * @default Configured with sane defaults.
+     */
+    tooMany5xxResponsesFromTargetsAlarm?: {
+      /**
+       * @default true
+       */
+      enabled?: boolean
+      /**
+       * An action to use for CloudWatch alarm state changes instead of the default action
+       */
+      action?: cloudwatch.IAlarmAction
+      /**
+       * @default 60 seconds
+       */
+      period?: cdk.Duration
+      /**
+       * @default 3
+       */
+      evaluationPeriods?: number
+      /**
+       * @default 10
+       */
+      threshold?: number
+      description?: string
+    }
+    /**
+     * Configuration for an alarm.
+     *
+     * @default Configured with sane defaults.
+     */
+    targetResponseTimeAlarm?: {
+      /**
+       * @default true
+       */
+      enabled?: boolean
+      /**
+       * An action to use for CloudWatch alarm state changes instead of the default action
+       */
+      action?: cloudwatch.IAlarmAction
+      /**
+       * @default 5 minutes
+       */
+      period?: cdk.Duration
+      /**
+       * @default 1
+       */
+      evaluationPeriods?: number
+      /**
+       * @default 500 milliseconds
+       */
+      threshold?: cdk.Duration
+      description?: string
+    }
   }): void {
-    const healthAlarm = new cloudwatch.Metric({
-      metricName: "HealthyHostCount",
-      namespace: "AWS/ApplicationELB",
-      statistic: "Average",
-      period: cdk.Duration.seconds(60),
-      dimensionsMap: {
-        TargetGroup: props.targetGroupFullName,
-        LoadBalancer: props.loadBalancerFullName,
-      },
-    }).createAlarm(this, "HealthAlarm", {
-      alarmDescription:
-        "Service might be unavailable! It is not responding to health checks.",
-      comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
-      evaluationPeriods: 1,
-      threshold: 1,
-      treatMissingData: cloudwatch.TreatMissingData.BREACHING,
-    })
-
-    healthAlarm.addAlarmAction(this.action)
-    healthAlarm.addOkAction(this.action)
-
-    const connectionAlarm = new cloudwatch.Metric({
+    const targetConnectionErrorAlarm = new cloudwatch.Metric({
       metricName: "TargetConnectionErrorCount",
       namespace: "AWS/ApplicationELB",
       statistic: "Sum",
-      period: cdk.Duration.seconds(60),
+      period: props.targetHealthAlarm?.period ?? cdk.Duration.seconds(60),
       dimensionsMap: {
         TargetGroup: props.targetGroupFullName,
         LoadBalancer: props.loadBalancerFullName,
       },
     }).createAlarm(this, "ConnectionAlarm", {
       actionsEnabled: true,
-      alarmDescription: "Load balancer could not connect to target",
-      evaluationPeriods: 1,
+      alarmDescription: `Load balancer is failing to connect to target(s) in ECS service '${this.serviceName}'.`,
+      evaluationPeriods: props.targetHealthAlarm?.evaluationPeriods ?? 1,
       threshold: 1,
       treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
     })
 
-    connectionAlarm.addAlarmAction(this.action)
-    connectionAlarm.addOkAction(this.action)
+    const healthAlarm = new cloudwatch.Metric({
+      metricName: "HealthyHostCount",
+      namespace: "AWS/ApplicationELB",
+      statistic: "Minimum",
+      period: props.targetHealthAlarm?.period ?? cdk.Duration.seconds(60),
+      dimensionsMap: {
+        TargetGroup: props.targetGroupFullName,
+        LoadBalancer: props.loadBalancerFullName,
+      },
+    }).createAlarm(this, "HealthAlarm", {
+      alarmDescription: `There are no healthy target(s) in ECS service '${this.serviceName}'.`,
+      comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
+      evaluationPeriods: props.targetHealthAlarm?.evaluationPeriods ?? 1,
+      threshold: 1,
+      treatMissingData: cloudwatch.TreatMissingData.BREACHING,
+    })
+
+    const targetHealthAlarm = new cloudwatch.CompositeAlarm(
+      this,
+      "TargetHealthAlarm",
+      {
+        alarmRule: cdk.aws_cloudwatch.AlarmRule.anyOf(
+          cdk.aws_cloudwatch.AlarmRule.fromAlarm(
+            targetConnectionErrorAlarm,
+            cloudwatch.AlarmState.ALARM,
+          ),
+          cdk.aws_cloudwatch.AlarmRule.fromAlarm(
+            healthAlarm,
+            cloudwatch.AlarmState.ALARM,
+          ),
+        ),
+        alarmDescription:
+          props.targetHealthAlarm?.description ??
+          `The load balancer is either receiving bad health checks from or is unable to connect to target(s) in ECS service '${this.serviceName}'`,
+        actionsEnabled: false,
+      },
+    )
+    if (props.targetHealthAlarm?.enabled ?? true) {
+      targetHealthAlarm.addAlarmAction(
+        props.targetHealthAlarm?.action ?? this.action,
+      )
+      targetHealthAlarm.addOkAction(
+        props.targetHealthAlarm?.action ?? this.action,
+      )
+    }
+
+    const tooMany5xxResponsesFromTargetsAlarm = new cloudwatch.Metric({
+      metricName: "HTTPCode_Target_5XX_Count",
+      namespace: "AWS/ApplicationELB",
+      statistic: "Sum",
+      period:
+        props.tooMany5xxResponsesFromTargetsAlarm?.period ??
+        cdk.Duration.seconds(60),
+      dimensionsMap: {
+        TargetGroup: props.targetGroupFullName,
+        LoadBalancer: props.loadBalancerFullName,
+      },
+    }).createAlarm(this, "AlbTargets5xxAlarm", {
+      actionsEnabled: true,
+      alarmDescription:
+        props.tooMany5xxResponsesFromTargetsAlarm?.description ??
+        `Load balancer received too many 5XX responses from target(s) in ECS service '${this.serviceName}'.`,
+      evaluationPeriods:
+        props.tooMany5xxResponsesFromTargetsAlarm?.evaluationPeriods ?? 3,
+      threshold: props.tooMany5xxResponsesFromTargetsAlarm?.threshold ?? 10,
+      treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+    })
+    if (props.tooMany5xxResponsesFromTargetsAlarm?.enabled ?? true) {
+      tooMany5xxResponsesFromTargetsAlarm.addAlarmAction(
+        props.tooMany5xxResponsesFromTargetsAlarm?.action ?? this.action,
+      )
+      tooMany5xxResponsesFromTargetsAlarm.addOkAction(
+        props.tooMany5xxResponsesFromTargetsAlarm?.action ?? this.action,
+      )
+    }
+
+    const targetResponseTimeAlarm = new cloudwatch.Metric({
+      metricName: "TargetResponseTime",
+      namespace: "AWS/ApplicationELB",
+      statistic: "p(95)",
+      period: props.targetResponseTimeAlarm?.period ?? cdk.Duration.minutes(5),
+      unit: cloudwatch.Unit.MILLISECONDS,
+      dimensionsMap: {
+        LoadBalancer: props.loadBalancerFullName,
+        TargetGroup: props.targetGroupFullName,
+      },
+    }).createAlarm(this, "TargetResponseTimeAlarm", {
+      alarmDescription:
+        props.targetResponseTimeAlarm?.description ??
+        `5% of responses from ECS service '${this.serviceName}' are taking longer than expected.`,
+      comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD,
+      evaluationPeriods: props.targetResponseTimeAlarm?.evaluationPeriods ?? 1,
+      threshold: (
+        props.targetResponseTimeAlarm?.threshold ?? cdk.Duration.millis(500)
+      ).toMilliseconds(),
+      treatMissingData: cloudwatch.TreatMissingData.IGNORE,
+    })
+    if (props.targetResponseTimeAlarm?.enabled ?? true) {
+      targetResponseTimeAlarm.addAlarmAction(
+        props.targetResponseTimeAlarm?.action ?? this.action,
+      )
+      targetResponseTimeAlarm.addOkAction(
+        props.targetResponseTimeAlarm?.action ?? this.action,
+      )
+    }
   }
 }
