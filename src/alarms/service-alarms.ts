932264ce7a25a2530a802b24b56c63b82e65c11e
stekern <stekern@users.noreply.github.com>
2023-06-20 17:18:55 +0200

    fix: use correct metric unit for alb target response time alarm

--- src/alarms/service-alarms.ts
+++ src/alarms/service-alarms.ts
@@ -295,7 +295,7 @@ export class ServiceAlarms extends constructs.Construct {
       namespace: "AWS/ApplicationELB",
       statistic: "p95",
       period: props.targetResponseTimeAlarm?.period ?? cdk.Duration.minutes(5),
-      unit: cloudwatch.Unit.MILLISECONDS,
+      unit: cloudwatch.Unit.SECONDS,
       dimensionsMap: {
         LoadBalancer: props.loadBalancerFullName,
         TargetGroup: props.targetGroupFullName,
@@ -308,7 +308,7 @@ export class ServiceAlarms extends constructs.Construct {
       evaluationPeriods: props.targetResponseTimeAlarm?.evaluationPeriods ?? 1,
       threshold: (
         props.targetResponseTimeAlarm?.threshold ?? cdk.Duration.millis(500)
-      ).toMilliseconds(),
+      ).toSeconds({ integral: false }),
       treatMissingData: cloudwatch.TreatMissingData.IGNORE,
     })
     if (props.targetResponseTimeAlarm?.enabled ?? true) {
