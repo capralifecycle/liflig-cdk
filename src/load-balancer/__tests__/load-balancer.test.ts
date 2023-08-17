5e2061f31ca7e20f1f6b900b6a2acad49a3c4286
Sirius Bakke <sirius@bakke.co>
2023-08-17 12:20:41 +0200

    feat: allow overriding HttpsListener props in LoadBalancer

--- src/load-balancer/__tests__/load-balancer.test.ts
+++ src/load-balancer/__tests__/load-balancer.test.ts
@@ -5,6 +5,7 @@ import * as route53 from "aws-cdk-lib/aws-route53"
 import { App, Stack } from "aws-cdk-lib"
 import "jest-cdk-snapshot"
 import { LoadBalancer } from ".."
+import { SslPolicy } from "aws-cdk-lib/aws-elasticloadbalancingv2"
 
 test("create load balancer", () => {
   const app = new App()
@@ -34,6 +35,44 @@ test("create load balancer", () => {
   new LoadBalancer(stack, "LoadBalancer", {
     certificates: [certificate],
     vpc: vpc,
+  })
+
+  expect(stack).toMatchCdkSnapshot()
+})
+
+test("create load balancer and override TLS configuration", () => {
+  const app = new App()
+
+  const supportStack = new Stack(app, "SuuportStack", {
+    env: {
+      region: "eu-north-1",
+    },
+  })
+
+  const stack = new Stack(app, "Stack", {
+    env: {
+      region: "eu-north-1",
+    },
+  })
+
+  const vpc = new ec2.Vpc(supportStack, "Vpc")
+
+  const hostedZone = new route53.HostedZone(supportStack, "HostedZone", {
+    zoneName: "2.example.com",
+  })
+
+  const certificate = new cm.Certificate(supportStack, "Certificate", {
+    domainName: `*.2.example.com`,
+    subjectAlternativeNames: ["2.example.com"],
+    validation: cm.CertificateValidation.fromDns(hostedZone),
+  })
+
+  new LoadBalancer(stack, "LoadBalancer", {
+    certificates: [certificate],
+    vpc: vpc,
+    overrideHttpsListenerProps: {
+      sslPolicy: SslPolicy.RECOMMENDED_TLS,
+    },
   })
 
   expect(stack).toMatchCdkSnapshot()
