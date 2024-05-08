df97eca5d0958780fb840f7d8fbba765676d2814
Kristian Rekstad <kre@capraconsulting.no>
2024-05-08 10:43:30 +0200

    feat: list all active alarms when sending updates to slack (#242)

--- assets/slack-alarm-lambda/index.py
+++ assets/slack-alarm-lambda/index.py
@@ -6,6 +6,7 @@ from urllib.error import URLError, HTTPError
 import boto3
 
 secrets_manager = boto3.client("secretsmanager")
+cloudwatch = boto3.client("cloudwatch")
 
 SLACK_URL_SECRET_NAME = os.getenv("SLACK_URL_SECRET_NAME", None)
 PROJECT_NAME = os.getenv("PROJECT_NAME", "undefined")
@@ -15,9 +16,24 @@ ENVIRONMENT_NAME = os.getenv("ENVIRONMENT_NAME", "undefined")
 def handler(event, context):
     print("Event: " + json.dumps(event))
     message = json.loads(event["Records"][0]["Sns"]["Message"])
-    region = event["Records"][0]["Sns"]["TopicArn"].split(":")[3]
+    topic_arn = event["Records"][0]["Sns"]["TopicArn"]
+    region = topic_arn.split(":")[3]
 
-    return send_slack_notification(message, region)
+    active_alarms = list_all_active_alarms(topic_arn)
+    return send_slack_notification(message, region, active_alarms)
+
+
+def list_all_active_alarms(topic_arn: str) -> list[str]:
+    try:
+        response: dict = cloudwatch.describe_alarms(AlarmTypes=["CompositeAlarm", "MetricAlarm"],
+                                                    StateValue="ALARM",
+                                                    ActionPrefix=topic_arn)
+        all_alarms = response["CompositeAlarms"] + response["MetricAlarms"]
+        names: list[str] = [alarm["AlarmName"] for alarm in all_alarms if alarm["ActionsEnabled"]]
+        return names
+    except Exception as e:
+        print(f"Failed to list alarms: {e}")
+        return []
 
 
 def get_masked_slack_webhook_url(slack_webhook_url: str):
@@ -37,7 +53,7 @@ def get_secret(secret):
         raise Exception(f"Error retrieving secret: {e}")
 
 
-def send_slack_notification(message, region):
+def send_slack_notification(message: str, region: str, active_alarms: list[str]):
     alarm_emojis = {
         "ALARM": ":rotating_light:",
         "INSUFFICIENT_DATA": ":warning:",
@@ -52,9 +68,9 @@ def send_slack_notification(message, region):
         {
             "color": color,
             "title_link": "https://console.aws.amazon.com/cloudwatch/home?region="
-            + region
-            + "#alarm:alarmFilter=ANY;name="
-            + message["AlarmName"],
+                          + region
+                          + "#alarm:alarmFilter=ANY;name="
+                          + message["AlarmName"],
             "fallback": f"{alarm_emojis.get(message['NewStateValue'], '')} {message['AlarmName']}: {alarm_description}",
             "fields": [
                 {"title": "Alarm Name", "value": message["AlarmName"], "short": False},
@@ -70,18 +86,23 @@ def send_slack_notification(message, region):
                 {
                     "title": "State Transition",
                     "value": message.get("OldStateValue", "Unknown")
-                    + " -> "
-                    + message["NewStateValue"],
+                             + " -> "
+                             + message["NewStateValue"],
                     "short": False,
                 },
                 {
                     "title": "Link to Alarm",
                     "value": "https://console.aws.amazon.com/cloudwatch/home?region="
-                    + region
-                    + "#alarm:alarmFilter=ANY;name="
-                    + message["AlarmName"],
+                             + region
+                             + "#alarm:alarmFilter=ANY;name="
+                             + message["AlarmName"],
                     "short": False,
                 },
+                {
+                    "title": "All active alarms " + (alarm_emojis["ALARM"] if len(active_alarms) else ""),
+                    "value": "\n".join(["- " + alarm for alarm in active_alarms]) if len(active_alarms) else "None",
+                    "short": False,
+                }
             ],
         }
     ]
