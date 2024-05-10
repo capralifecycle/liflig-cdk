a00c6c50dc3ca45e0d9121ac65c90291039b9b60
Kristian Rekstad <kre@capraconsulting.no>
2024-05-10 16:11:47 +0200

    test: verify database-alarms throws on non-burstable with cpu credit alarm

--- src/alarms/__tests__/database-alarms.test.ts
+++ src/alarms/__tests__/database-alarms.test.ts
@@ -5,6 +5,7 @@ import * as sns from "aws-cdk-lib/aws-sns"
 import * as ec2 from "aws-cdk-lib/aws-ec2"
 import "jest-cdk-snapshot"
 import { DatabaseAlarms } from "../database-alarms"
+import { throws } from "node:assert"
 
 test("create alarms", () => {
   const app = new App()
@@ -32,4 +33,35 @@ test("create alarms", () => {
   alarms.addStorageSpaceAlarms()
 
   expect(stack).toMatchCdkSnapshot()
+})
+
+test("throws on non-burstable", () => {
+  // Given
+  const app = new App()
+  const supportStack = new Stack(app, "SupportStack")
+  const stack = new Stack(app, "Stack")
+
+  const topic = new sns.Topic(supportStack, "Topic")
+  const action = new cloudwatchActions.SnsAction(topic)
+
+  const alarms = new DatabaseAlarms(stack, "DatabaseAlarms", {
+    instanceIdentifier: "database-name",
+    instanceType: ec2.InstanceType.of(
+      ec2.InstanceClass.M6G /* Not burstable! */,
+      ec2.InstanceSize.LARGE,
+    ),
+    allocatedStorage: Size.gibibytes(25),
+    action,
+  })
+
+  // Then
+  throws(
+    () => {
+      // When
+      alarms.addCpuCreditsAlarm()
+    },
+    {
+      message: /only relevant for burstable/,
+    },
+  )
 })
