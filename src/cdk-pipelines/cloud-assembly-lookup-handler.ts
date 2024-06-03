77bb3f73f7520213aa2e1cbb24e8870163ed6678
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-06-03 11:13:37 +0200

    fix: rewrite handler to support streams

--- src/cdk-pipelines/cloud-assembly-lookup-handler.ts
+++ src/cdk-pipelines/cloud-assembly-lookup-handler.ts
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import type { Handler } from "aws-lambda"
-import {
-  GetObjectCommand,
-  PutObjectCommand,
-  S3Client,
-} from "@aws-sdk/client-s3"
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3"
 import {
   CodePipelineClient,
   PutJobFailureResultCommand,
   PutJobSuccessResultCommand,
 } from "@aws-sdk/client-codepipeline"
+import { Upload } from "@aws-sdk/lib-storage"
 
 // Relevant fields from
 // https://docs.amazonaws.cn/en_us/lambda/latest/dg/services-codepipeline.html
@@ -64,18 +61,22 @@ export const handler: Handler = async (event: CodePipelineEvent, context) => {
 
     console.log("User parameters", userParametersRaw)
 
+    console.log("Parsing user parameters")
     const userParameters = JSON.parse(
       userParametersRaw,
     ) as CloudAssemblyLookupUserParameters
 
+    console.log("Fetching reference data from S3")
     const getReferenceDataResp = await s3Client.send(
       new GetObjectCommand({
         Bucket: userParameters.bucketName,
         Key: userParameters.objectKey,
       }),
     )
-    const referenceData = getReferenceDataResp.Body!.toString()
+    const referenceData =
+      await getReferenceDataResp.Body!.transformToString("utf-8")
 
+    console.log("Parsing reference data from S3")
     const cloudAssemblyReference = JSON.parse(
       referenceData,
     ) as CloudAssemblyReference
@@ -83,26 +84,47 @@ export const handler: Handler = async (event: CodePipelineEvent, context) => {
     const outputArtifact = event["CodePipeline.job"].data.outputArtifacts[0]
     const s3Loc = outputArtifact.location.s3Location
 
+    console.log("Fetching Cloud Assembly ZIP data from S3")
     const cloudAssemblyZipData = await s3Client.send(
       new GetObjectCommand({
         Bucket: cloudAssemblyReference.cloudAssemblyBucketName,
         Key: cloudAssemblyReference.cloudAssemblyBucketKey,
       }),
     )
-    console.log("Size of Cloud Assembly", cloudAssemblyZipData.ContentLength)
+    console.log(
+      "Content-Length of Cloud Assembly (bytes)",
+      cloudAssemblyZipData.ContentLength,
+    )
 
+    console.log("Creating authenticated S3 client for artifact upload")
+    const artifactCreds = event["CodePipeline.job"].data.artifactCredentials
+    if (artifactCreds === undefined) {
+      console.error("No artifact credentials found in job event")
+    }
     const authedS3Client = new S3Client({
-      credentials: event["CodePipeline.job"].data.artifactCredentials,
+      credentials: artifactCreds,
     })
 
-    await authedS3Client.send(
-      new PutObjectCommand({
+    console.log(
+      `Uploading Cloud Assembly ZIP to s3://${s3Loc.bucketName}/${s3Loc.objectKey}`,
+    )
+
+    const upload = new Upload({
+      client: authedS3Client,
+      params: {
         Bucket: s3Loc.bucketName,
         Key: s3Loc.objectKey,
         Body: cloudAssemblyZipData.Body,
-      }),
-    )
+      },
+    })
 
+    upload.on("httpUploadProgress", (progress) => {
+      console.log(progress)
+    })
+
+    await upload.done()
+
+    console.log("Sending success result to CodePipeline")
     await codepipelineClient.send(
       new PutJobSuccessResultCommand({
         jobId,
@@ -110,6 +132,7 @@ export const handler: Handler = async (event: CodePipelineEvent, context) => {
     )
     console.log("Success")
   } catch (e) {
+    console.log("Sending failure result to CodePipeline")
     await codepipelineClient.send(
       new PutJobFailureResultCommand({
         failureDetails: {
