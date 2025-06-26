14e8967cddf35f04579db9f9b2b44dc1b4b9fbc2
Sirius Bakke <sirius@bakke.co>
2025-06-26 14:24:42 +0200

    fix: set default expire rules for ecr images

--- src/build-artifacts/index.ts
+++ src/build-artifacts/index.ts
@@ -30,7 +30,7 @@ interface Props {
   /**
    * The lifecycle rules to apply to images stored in the ECR repository.
    *
-   * @default - Expire untagged images and images used for CI caching after 10 days
+   * @default - Expire untagged images after 10 days and any image older than 180 days
    */
   ecrRepositoryLifecycleRules?: ecr.LifecycleRule[]
   /**
@@ -197,12 +197,11 @@ export class BuildArtifacts extends constructs.Construct {
       lifecycleRules: props.ecrRepositoryLifecycleRules || [
         {
           maxImageAge: cdk.Duration.days(10),
-          tagPrefixList: ["ci-cache-"],
-          tagStatus: ecr.TagStatus.TAGGED,
+          tagStatus: ecr.TagStatus.UNTAGGED,
         },
         {
-          maxImageAge: cdk.Duration.days(10),
-          tagStatus: ecr.TagStatus.UNTAGGED,
+          maxImageAge: cdk.Duration.days(180),
+          tagStatus: ecr.TagStatus.ANY,
         },
       ],
     })
