58c173c457a0b716a0a04f72dde1d5f6a2944235
evan-boone <112849663+evan-boone@users.noreply.github.com>
2026-03-11 12:45:06 +0100

    feat: add logging of error content to Slack (#387)

--- src/api-gateway/__tests__/http-api-gateway.test.ts
+++ src/api-gateway/__tests__/http-api-gateway.test.ts
@@ -258,7 +258,7 @@ describe("HTTP API Gateway", () => {
         `exports.handler = async(event) => { return "Hello World"; }`,
       ),
       handler: "index.handler",
-      runtime: Runtime.NODEJS_24_X,
+      runtime: Runtime.NODEJS_LATEST,
     })
 
     new ApiGateway(stack, "TestLambdaApiGateway", {
